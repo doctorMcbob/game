47f196bd98f85380f366ccc18957784658e31dac
doctormcbob <wootenwesley@gmail.com>
2019-03-16 09:55:45 -0700

    seperated game pieces from demo

--- gamepieces.py
+++ gamepieces.py
@@ -14,10 +14,8 @@ import pygame
 from pygame.locals import *
 import sys, os
 DEBUG = 'debug' in sys.argv
-
 pygame.init()
 FONT = pygame.font.SysFont("helvetica", 10)
-CLOCK = pygame.time.Clock()
 
 class GamePiece(pygame.rect.Rect):
 	"Anything with the daunting task of being in the game"
@@ -192,73 +190,3 @@ class Enemy(Trigger):
 	def draw(self, destination):
 		GamePiece.draw(self, destination)
 
-if __name__ == "__main__":
-	# TEST ROOM :)
-	SCOREKEEP = pygame.font.SysFont("helvetica", 40)
-	beerpile = [(100, 440), (300, 440), (500, 440), (300, 280)]
-	def advance_frame(gameboard, CLOCK, SCREEN):
-		CLOCK.tick(30)
-		if DEBUG: os.system("clear||cls")
-		if not gameboard["collectables"]: 
-			gameboard["collectables"] = [GamePiece(x, y, 20, 20, name="beer", tangible=False, color=(210, 180, 200)) for (x, y) in beerpile]
-		for peice in gameboard["platforms"] + [gameboard["player"]] + gameboard["triggers"] + gameboard["collectables"]:
-			peice.advance(gameboard)
-			peice.draw(SCREEN)
-		SCREEN.blit(SCOREKEEP.render("Beers drank: "+str(len(gameboard["player"].collectables)), 0, (0, 0, 0)), (50, 50))
-
-
-	SCREEN = pygame.display.set_mode((640, 480))
-	PLAYER = {
-		"name": "player",
-		"color": (100, 50, 100),
-		"grav": 1,
-		"jump_vel": -15,
-		"jumps": 2,
-		"dash": True,
-		"dash_speed": 14,
-		"dash_frames": 25,
-		"walk_speed": 8,
-		"speed": 1,
-		"friction": 1,
-	}
-
-	def walker(self, game):
-		self.direction = -1 if self.x > game["player"].x else 1
-		self.x_vel = self.speed * self.direction
-	def kill(self, game):
-		game["player"] = Player(50, 50, 30, 40, **PLAYER)
-		self.triggered = False
-	COP = {
-		"name": "granny",
-		"color": (250, 55, 55),
-		"direction": 1,
-		"speed": 3,
-		"trigger_function": kill,
-		"brain_function": walker
-	}	
-
-
-	wall = GamePiece(200, 320, 20, 140, name="door", tangible=True, color=(180, 150, 120))
-	def door(self, game):
-		order = [(200, 320),(200, 180),(420, 180),(420, 320)]
-		self.x, self.y = wall.x, wall.y
-		self.wall.x, self.wall.y = order[(order.index((wall.x, wall.y)) + 1) % 4]
-		self.triggered = False
-
-	TRIGGER = {
-		"trigger_function": door,
-		"name": "doortrigger",
-		"color": (20, 150, 50),
-		"wall": wall
-	}
-	gameboard = {
-		"player": Player(50, 50, 30, 40, **PLAYER),
-		"platforms": [wall] + [GamePiece(x, y, w, h, name="platform", color=(150, 150, 100))
-		for x, y, w, h in [(0, 460, 640, 20), (620, 0, 20, 460), (0, 0, 20, 460), (200, 300, 240, 20)]],
-		"triggers": [Trigger(420, 320, 20, 140, **TRIGGER), Enemy(600, 420, 30, 40, **COP)],
-		"collectables": [GamePiece(x, y, 20, 20, name="beer", color=(210, 180, 200)) for (x, y) in beerpile]
-	}
-	while True:
-		SCREEN.fill((255, 255, 255))
-		advance_frame(gameboard, CLOCK, SCREEN)
-		pygame.display.update()
